d45c7e9a98e0b6c97eb754a5cc545c7d3f4c3697
Jesus Kauze <jesuskauze@gmail.com>
2017-11-18 14:14:00 -0400

    header mejorado con pestanas: resultado, saldo, perfil

---
+++ app/controllers/cp_controller.rb
@@ -0,0 +1,19 @@
+class CpController < ApplicationController
+
+	#Pagina principal para apostar
+	def jugar
+	end
+
+	#Pagina principal para recargar y ver saldo
+	def saldo
+	end
+
+	#Pagina para modificar y ver perfil
+	def perfil
+	end
+
+	#Pagina con los resultados del dia
+	def resultados
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/conocenos'
   get 'welcome/aprende'
+  get 'cp/jugar'
+  get 'cp/perfil'
+  get 'cp/saldo'
+  get 'cp/resultados'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
