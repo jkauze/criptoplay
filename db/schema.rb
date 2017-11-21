655427a39b74267fd7112a23357c59b9c0d3c3ff
josedavidb <josedavidb6@gmail.com>
2017-11-21 12:55:06 -0400

    Integracion de Frontend con template. Menu listo

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171119152739) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "coin_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.float    "saldo"
@@ -61,6 +62,7 @@ ActiveRecord::Schema.define(version: 20171119152739) do
     t.string   "name"
   end
 
+  add_index "users", ["coin_id"], name: "index_users_on_coin_id"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
